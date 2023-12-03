dd78a8cc4378acc578a8f02410fe9b652b6caabf
Carrick Bartle <carrickdb@gmail.com>
2023-12-02 22:29:48 -0800

    day 3

--- 2.py
+++ 2.py
@@ -1,34 +1,15 @@
-## Part 2
-total = 0
-with open("input2") as f:
-    for l in f:
-        data = l.strip().split(": ")
-        fewest = {"red":0, "green":0, "blue":0}
-        draws = data[1].split("; ")
-        for d in draws:
-            cubes = d.split(", ")
-            for c in cubes:
-                num, color = c.split(" ")
-                num = int(num)
-                fewest[color] = max(fewest[color], num)
-        power = 1
-        for num in fewest.values():
-            power *= num
-        total += power
+# Regex redux
 
-print(total)
+import re
 
-
-## Part 1
-# 12 red cubes, 13 green cubes, and 14 blue cubes
 target = {"red":12, "green":13, "blue":14}
 possible = 0
 # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+pattern = re.compile(r'Game (\d+): (?:\d+ (\b)(?:)(?:;|$))+')
 with open("input") as f:
     for l in f:
-        data = l.strip().split(": ")
-        id = int(data[0].split(" ")[1])
-        draws = data[1].split("; ")
+        matches = pattern.findall(l.strip())
+
         ok = True
         for d in draws:
             cubes = d.split(", ")
@@ -44,3 +25,52 @@ with open("input") as f:
             possible += id
 
 print(possible)
+
+
+
+# ## Part 2
+# total = 0
+# with open("input2") as f:
+#     for l in f:
+#         data = l.strip().split(": ")
+#         fewest = {"red":0, "green":0, "blue":0}
+#         draws = data[1].split("; ")
+#         for d in draws:
+#             cubes = d.split(", ")
+#             for c in cubes:
+#                 num, color = c.split(" ")
+#                 num = int(num)
+#                 fewest[color] = max(fewest[color], num)
+#         power = 1
+#         for num in fewest.values():
+#             power *= num
+#         total += power
+
+# print(total)
+
+
+# ## Part 1
+# # 12 red cubes, 13 green cubes, and 14 blue cubes
+# target = {"red":12, "green":13, "blue":14}
+# possible = 0
+# # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+# with open("input") as f:
+#     for l in f:
+#         data = l.strip().split(": ")
+#         id = int(data[0].split(" ")[1])
+#         draws = data[1].split("; ")
+#         ok = True
+#         for d in draws:
+#             cubes = d.split(", ")
+#             for c in cubes:
+#                 num, color = c.split(" ")
+#                 num = int(num)
+#                 if num > target[color]:
+#                     ok = False
+#                     break
+#             if not ok:
+#                 break
+#         if ok:
+#             possible += id
+
+# print(possible)
---
+++ 3.py
@@ -0,0 +1,80 @@
+import aoc, re
+
+total = 0
+grid = []
+with open("input") as f:
+    for l in f:
+        grid.append(list(l.strip()))
+
+for i in range(len(grid)):
+    row = grid[i]
+    for j in range(len(row)):
+        c = row[j]
+        if c == "*":
+            nums = []
+            newGrid = grid[max(0, i-1):min(len(grid), i+2)].copy()
+            ni = 1
+            if i-1 < 0:
+                ni = 0
+            for di, dj in aoc.diagDirs:
+                ip = ni+di
+                if ip < 0 or ip >= len(newGrid):
+                    continue
+                jp = j+dj
+                if jp < 0 or jp >= len(newGrid[0]):
+                    continue
+                newSpace = newGrid[ip][jp]
+                if newSpace.isdigit():
+                    beginning = jp
+                    end = jp
+                    while beginning >= 0 and newGrid[ip][beginning].isdigit():
+                        beginning -= 1
+                    while end < len(grid[0]) and newGrid[ip][end].isdigit():
+                        end += 1
+                    nums.append(int(''.join(newGrid[ip][beginning+1:min(len(grid[0]), end)])))
+                    for k in range(beginning+1, end):
+                        newGrid[ip][k] = '.'
+            if len(nums) == 2:
+                total += nums[0] * nums[1]
+                
+print(total)
+
+
+## Part 1
+
+total = 0
+grid = []
+with open("input") as f:
+    for l in f:
+        grid.append(l.strip())
+
+for i in range(len(grid)):
+    row = grid[i]
+    currNum = ""
+    partNumber = False
+    for j in range(len(row)):
+        c = row[j]
+        if c.isdigit():
+            currNum += c
+            if partNumber == False:
+                for di, dj in aoc.diagDirs:
+                    ip = i+di
+                    jp = j+dj
+                    if ip < 0 or ip >= len(grid):
+                        continue
+                    if jp < 0 or jp >= len(grid[0]):
+                        continue
+                    newSpace = grid[ip][jp]
+                    if not newSpace.isdigit() and newSpace != ".":
+                        partNumber = True
+                        break
+        elif currNum != "":
+            if partNumber:
+                total += int(currNum)
+                partNumber = False
+            currNum = ""
+    if partNumber:
+        total += int(currNum)
+
+
+print(total)
