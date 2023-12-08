62da03375b7f58eb78425f641f6d9146a6e34c48
Carrick Bartle <carrickdb@gmail.com>
2023-12-07 22:29:28 -0800

    day 8 part 2

--- 8.py
+++ 8.py
@@ -1,4 +1,4 @@
-import aoc, re, queue
+import aoc, re, queue, math
 
 # RL
 
@@ -25,17 +25,18 @@ q = queue.Queue()
 for edge in edges.keys():
     if edge[2] == "A":
         q.put(edge)
+patterns = [-1 for i in range(q.qsize())]
 while not q.empty():
     # print(list(q.queue))
     lenq = q.qsize()
     found = False
-    numFound = 0
     for i in range(lenq):
         curr = q.get()
         # print("curr", curr)
-        if curr[2] == "Z":
-            numFound += 1
-            if numFound == lenq:
+        if curr[2] == "Z" and patterns[i] == -1:
+            patterns[i] = steps
+            print(patterns)
+            if all([True if x>0 else False for x in patterns]):
                 found = True
                 break
         currDir = dirs[steps % len(dirs)]
@@ -45,7 +46,58 @@ while not q.empty():
         break
     steps += 1
 
-print(steps)
+print(math.lcm(*patterns))
+
+
+
+# import aoc, re, queue
+
+# # RL
+
+# # AAA = (BBB, CCC)
+
+# edges = {}
+
+# with open("input") as f:
+#     dirs = f.readline().strip()
+#     f.readline()
+#     for l in f:
+#         pattern = re.compile("([0-9A-Z][0-9A-Z][0-9A-Z]) = \(([0-9A-Z][0-9A-Z][0-9A-Z]), ([0-9A-Z][0-9A-Z][0-9A-Z])\)")
+#         matches = pattern.findall(l.strip())[0]
+#         if len(matches) != 3:
+#             print(matches)
+#         edges[matches[0]] = [matches[1], matches[2]]
+
+# mapping = {
+#     "L": 0,
+#     "R": 1
+# }
+# steps = 0
+# q = queue.Queue()
+# for edge in edges.keys():
+#     if edge[2] == "A":
+#         q.put(edge)
+# while not q.empty():
+#     # print(list(q.queue))
+#     lenq = q.qsize()
+#     found = False
+#     numFound = 0
+#     for i in range(lenq):
+#         curr = q.get()
+#         # print("curr", curr)
+#         if curr[2] == "Z":
+#             numFound += 1
+#             if numFound == lenq:
+#                 found = True
+#                 break
+#         currDir = dirs[steps % len(dirs)]
+#         # print("currdir", currDir)
+#         q.put(edges[curr][mapping[currDir]])
+#     if found:
+#         break
+#     steps += 1
+
+# print(steps)
 
 
 
