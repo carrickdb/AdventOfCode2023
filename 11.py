9789017accaf482eee6185d56879d7380099fb1f
Carrick Bartle <carrickdb@gmail.com>
2023-12-11 18:47:42 -0800

    Day 11

--- 11.py
+++ 11.py
@@ -1,72 +1,55 @@
-import numpy, queue, aoc, copy
-
 def printGrid(g):
     for l in g:
         print(l)
 
-u = []
+galaxies = []
+
+emptyRows = []
+emptyCols = []
+g = []
 with open("input") as f:
     i = 0
     for l in f:
         l = l.strip()
-        u.append(list(l))
+        g.append(list(l))
         if all([x=="." for x in l]):
-            u.append(list(l))
+            emptyRows.append(i)
+        else:
+            galaxies.extend([(i, j) for j in range(len(l)) if l[j]=="#"])
+        i += 1
 
-u = numpy.transpose(u)
+newg = []
+for j in range(len(g[0])):
+    newRow = []
+    for i in range(len(g)):
+        newRow.append(g[i][j])
+    newg.append(newRow)
 
-u2 = []
-for row in u:
-    u2.append(row)
-    if all([x=="." for x in row]):
-        u2.append(row.copy())
-
-u = numpy.transpose(u2)
-printGrid(u)
-
-umap = {}
-for i in range(len(u)):
-    umap[i] = {}
-    for j in range(len(u[0])):
-        umap[i][j] = u[i][j]
+i = 0
+for l in newg:
+    if all([x=="." for x in l]):
+        emptyCols.append(i)
+    i += 1
 
 total = 0
-
-paths = set()
-
-def getPaths(g, i, j):
-    q = queue.Queue()
-    q.put((i,j))
-    currtotal = 0
-    steps = 0
-    while not q.empty():
-        # aoc.printMapGrid(g)
-        # print(list(q.queue))
-        lenq = q.qsize()
-        for _ in range(lenq):
-            ci, cj = q.get()
-            if g[ci][cj] == "#":
-                path = ((ci, cj), (i, j))
-                if path not in paths:
-                    currtotal += steps
-                    paths.add(path)
-                    paths.add(((i,j), (ci, cj)))
-            if g[ci][cj] == "X":
-                continue
-            g[ci][cj] = "X"
-            for di, dj in aoc.dirs:
-                ni, nj = di+ci, dj+cj
-                if ni in g and nj in g[ni]:
-                    if g[ni][nj] != "X":
-                        q.put((ni, nj))
-        steps += 1
-    return currtotal
-
-for i in range(len(umap)):
-    for j in range(len(umap[0])):
-        curr = umap[i][j]
-        if curr == '#':
-            total += getPaths(copy.deepcopy(umap), i, j)
-            print(total)
+totals = {}
+for first in range(len(galaxies)):
+    for other in range(first+1, len(galaxies)):
+        n, m = galaxies[other]
+        i, j = galaxies[first]
+        temptotal = abs(i-n) + abs(j-m)
+        minx = min(j,m)
+        maxx = max(j,m)
+        miny = min(i,n)
+        maxy = max(i,n)
+        for curr in emptyRows:
+            if curr > miny and curr < maxy:
+                temptotal += 1000000-1
+        for curr in emptyCols:
+            if curr > minx and curr < maxx:
+                temptotal += 1000000-1
+        total += temptotal
+        totals[(first+1, other+1)] = temptotal
 
 print(total)
+print(len(galaxies))
